941e05cb523dcf46467536ffd69ce22748ced857
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2026-02-01 03:17:01 -0500

    adding example dir and binary

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,24 +1,6 @@
-import { useState } from 'react'
-import { Dialog } from './components/Dialog'
-import { Button } from './components/Button'
 
 const Application = () => {
-	const [open, setOpen] = useState(false)
-
-	return (<>
-		<h1>Hello World</h1>
-		<Button
-			label="Open Dialog"
-			onClick={() => setOpen(true)}
-		/>
-		<Dialog
-			onClose={() => setOpen(false)}
-			open={open}
-			title="My Dialog"
-		>
-			<p>This is the content of the dialog.</p>
-		</Dialog>
-	</>)
+	return <span>Hello World</span>
 }
 
 export default Application
